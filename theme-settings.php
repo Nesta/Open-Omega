951d85d13f684e8eda972bba88dcc15b9e2b0542
@himerus <jake.strawn@gmail.com>
2011-02-26 14:04:34 -0500

    cleanup theme settings form to remove unused/irrelevant settings

--- theme-settings.php
+++ theme-settings.php
@@ -14,5 +14,18 @@
  *   A keyed array containing the current state of the form.
  */
 function open_omega_form_system_theme_settings_alter(&$form, &$form_state) {
-  // Include any changes to the theme settings here. 
+  // Remove some sections of the theme settings that are irrelevant for Open Omega
+  unset($form['omega_general']['menu']);
+  unset($form['omega_general']['grid_overlay']);
+  unset($form['omega_general']['breadcrumb']);
+  // get rid of the option to enable Omega core CSS that will mess things up
+  unset($form['omega_general']['optional_css']['text_css']);
+  unset($form['omega_general']['optional_css']['table_css']);
+  unset($form['omega_general']['optional_css']['menu_css']);
+  unset($form['omega_general']['optional_css']['link_css']);
+  unset($form['omega_general']['optional_css']['form_css']);
+  unset($form['omega_general']['optional_css']['pager_css']);
+  unset($form['omega_general']['optional_css']['regions_css']);
+  unset($form['omega_general']['optional_css']['defaults_css']);
+  unset($form['omega_general']['optional_css']['custom_css']);
 }
